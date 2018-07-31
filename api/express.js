a8de7f16a88868fa75ab8bcf2985f41c26ad9f0b
Alex Brazier <brazierale@gmail.com>
2018-07-31 13:23:54 +0100

    Updated API to use parameterised statements

--- api/express.js
+++ api/express.js
@@ -18,7 +18,14 @@ app.use((req, res, next) => {
 app.get('/api/testCases', (req, res) => {
     let sql = `SELECT * FROM ${TESTCASE} FOR JSON AUTO`;
 
-    executeSql(sql, (result) => {
+    let request = new Request(sql, (err) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            result = err;
+        }
+    });
+
+    executeSql(request, (result) => {
         // should only log this when any actual result is returned
         console.log('Returned test cases: ' + result);
         res.send({ express: result });
@@ -44,33 +51,60 @@ app.get('/api/testCases/:id', (req, res) => {
     });
 });
 
-app.put('/api/testCases/:id', (req, res) => {
-    let newSummary = req.body.summary;
-    let sql = `UPDATE ${TESTCASE} SET Summary='${newSummary}' WHERE ID = ${req.params.id}`;
-
-    executeSql(sql, (result) => {
-        // should only log this when any actual result is returned
-        console.log(`Updated test case:  ${req.params.id}: ${newSummary}`);
-        res.send({ express: result });
-    });
-});
-
 app.post('/api/testCases', (req, res) => {
     let summary = req.body.summary;
-    let sql = `INSERT INTO ${TESTCASE} VALUES ('${summary}')`;
+    let sql = `INSERT INTO ${TESTCASE} VALUES (@summary)`;
 
-    executeSql(sql, (result) => {
+    let request = new Request(sql, (err) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            result = err;
+        }
+    });
+
+    request.addParameter('summary', TYPES.VarChar, req.body.summary);
+    
+    executeSql(request, (result) => {
         // need to only return the message when successfully added
         console.log(`Added test case:  ${summary}`);
         res.send({ express: result });
     });
 });
 
-app.delete('/api/testCases/:id', (req, res) => {
-    let sql = `DELETE FROM ${TESTCASE} WHERE ID = ${req.params.id}`;
-    console.log(`Deleting... ${sql}`);
+app.put('/api/testCases/:id', (req, res) => {
+    let newSummary = req.body.summary;
+    let sql = `UPDATE ${TESTCASE} SET Summary = @summary WHERE ID = @id`;
 
-    executeSql(sql, (result) => {
+    let request = new Request(sql, (err) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            result = err;
+        }
+    });
+
+    request.addParameter('id', TYPES.VarChar, req.params.id);
+    request.addParameter('summary', TYPES.VarChar, req.body.summary);
+
+    executeSql(request, (result) => {
+        // should only log this when any actual result is returned
+        console.log(`Updated test case:  ${req.params.id}: ${newSummary}`);
+        res.send({ express: result });
+    });
+});
+
+app.delete('/api/testCases/:id', (req, res) => {
+    let sql = `DELETE FROM ${TESTCASE} WHERE ID = @id`;
+
+    let request = new Request(sql, (err) => {
+        if (err) {
+            console.log(`Error: ${err}`);
+            result = err;
+        }
+    });
+
+    request.addParameter('id', TYPES.VarChar, req.params.id);
+    
+    executeSql(request, (result) => {
         // need to only return the message when successfully deleted
         console.log(`Deleted test case: ${req.params.id}`);
         res.send({ express: result });
