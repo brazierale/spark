76eb3de181293d042c62e0f482a0e7572f58b264
Alex Brazier <brazierale@gmail.com>
2018-08-02 13:29:51 +0100

    Removed unnecessary addComponent method

--- client/src/display/DisplayInput.js
+++ client/src/display/DisplayInput.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Row } from './Row';
 import { Input } from './Input';
-import { TestCaseInput } from './TestCaseInput';
 import './Display.css'
 
 export class DisplayInput extends Component {
@@ -12,30 +10,14 @@ export class DisplayInput extends Component {
             rows: [],
         };
 
-        this.addComponent = this.addComponent.bind(this);
     }
     render() {
         return (
             <div className="Display-input">
                 {this.state.rows}
-                <Input addComponent={this.addComponent} createTestCase={this.props.createTestCase}/>
+                <Input createTestCase={this.props.createTestCase}/>
             </div>
         )
-    }
-
-    addComponent(text, id) {
-        console.log(`Adding component ${id}, ${text}`)
-        var newArray = this.state.rows.slice();
-        var row;
-
-        row = (
-            <Row key={id}>
-                <TestCaseInput summary={text}/>
-            </Row>
-        );
-
-        newArray.push(row);
-        this.setState({ rows: newArray });
     }
 }
 
