6cd912ecc6643df39884ff17ad750ee6a71e30d9
Alex Brazier <brazierale@gmail.com>
2018-08-27 17:04:07 +0100

    updated hello world test to use Alias

--- cypress/integration/first_spark.spec.js
+++ cypress/integration/first_spark.spec.js
@@ -1,18 +1,27 @@
 describe('Hello World test', () => {
 
-    it('New test case Hello World', () => {
+    beforeEach(() => {
         cy.visit('/')
+        cy.get('.Row').last().as('lastRow')
+    })
 
-        cy.get('.Test-case-container .Test-case').last()
-            .type('Hello World!')
-            .should('have.value', 'Hello World!')
+    it('Last row is the new row', () => {
 
-        cy.get('.Row .Row-id').last().contains(0)
-
-        cy.get('.Row').last().within((row) => {
+        cy.get('@lastRow').within(() => {
             cy.get('.Row-id')
                 .contains(0)
         })
+       
+    })
+    
+    it('After entering text, text is displayed', () => {
+
+        cy.get('@lastRow').within(() => {
+            cy.get('.Test-case')
+                .type('Hello World!')
+                .should('have.value', 'Hello World!')
+        })
 
     })
+
 })
