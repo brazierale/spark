9f7fdd6df50090c637f1ab738e19266d457e3655
Alex Brazier <brazierale@gmail.com>
2018-08-28 12:29:25 +0100

    added test for deleting so whole test can be rerun

--- cypress/integration/first_spark.spec.js
+++ cypress/integration/first_spark.spec.js
@@ -14,14 +14,43 @@ describe('Hello World test', () => {
        
     })
     
-    it('After entering text, text is displayed', () => {
+    it('Detail pane displays entered text after creating a new test', () => {
 
         cy.get('@lastRow').within(() => {
             cy.get('.Test-case')
                 .type('Hello World!')
-                .should('have.value', 'Hello World!')
+                .type('{Enter}')
         })
+
+        cy.get('.Test-case[value="Hello World!"]')
+            .click()
+
+        cy.get('.Detail-pane-header h1')
+            .contains('Hello World!')
+
+        // get the detail pane summary and check it matches the entered text
 
     })
 
+    it('Delete button deletes selected test case', () => {
+
+        cy.get('.Test-case[value="Hello World!"]').parent().parent().as('rowToDelete')
+
+        // confirm delete icon appears on hover, doesn't work due to using CSS for styling
+        // cy.get('@rowToDelete').trigger('mouseover')
+        // cy.get('@rowToDelete').within(() => {
+        //     cy.get('.Delete-row').should('be.visible')
+        // })
+
+        // select the delete icon, force as there is no 'hover' command in cypress
+        cy.get('@rowToDelete').within(() => {
+            cy.get('.Delete-row')
+                .click({force:true})
+        })
+
+        cy.get('.Test-case[value="Hello World!"]').should('not.exist')
+
+    })
+
+    // ***TODO: edge case checking only the row delete was performed on was deleted, all other tests should remain***
 })
