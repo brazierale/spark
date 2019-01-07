7aaeea8d81c11b9db9ec49d1c2a4fcfc759f730b
Alex Brazier <alex@Alexs-MacBook-Pro.local>
2019-01-07 14:05:05 +0000

    migrated from MSSQL to mongodb

---
+++ cypress/integration/test_cypress.js
@@ -0,0 +1,60 @@
+describe('cypress test', function() {
+
+    beforeEach(function() {
+        // reset data to 1 new item
+        cy.exec('node api/drop-data.js')
+
+        cy.visit('/')
+        
+        cy.get('.Row').last().as('lastRow')
+
+        cy.get('@lastRow').within(() => {
+            cy.get('.Test-case')
+                .type('Hello World!')
+                .type('{Enter}')
+        })
+    })
+
+    it('Last row is the new row', () => {
+
+        cy.get('@lastRow').within(() => {
+            cy.get('.Row-id')
+                .contains(0)
+        })
+       
+    })
+
+    it('Detail pane displays entered text after creating a new test', () => {
+
+        cy.get('.Test-case[value="Hello World!"]')
+            .click()
+
+        cy.get('.Detail-pane-header h1')
+            .contains('Hello World!')
+
+        // get the detail pane summary and check it matches the entered text
+
+    })
+
+    it('Delete button deletes selected test case', () => {
+
+        cy.get('.Test-case[value="Hello World!"]').parent().parent().as('rowToDelete')
+
+        // confirm delete icon appears on hover, doesn't work due to using CSS for styling
+        // cy.get('@rowToDelete').trigger('mouseover')
+        // cy.get('@rowToDelete').within(() => {
+        //     cy.get('.Delete-row').should('be.visible')
+        // })
+
+        // select the delete icon, force as there is no 'hover' command in cypress
+        cy.get('@rowToDelete').within(() => {
+            cy.get('.Delete-row')
+                .click({force:true})
+        })
+
+        cy.get('.Test-case[value="Hello World!"]').should('not.exist')
+
+    })
+
+    // ***TODO: edge case checking only the row delete was performed on was deleted, all other tests should remain***
+})
